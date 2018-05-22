b2fe3540426154be18df357ec9c59d6a16a29bad
drakendrast <37454203+drakendrast@users.noreply.github.com>
2018-05-22 02:49:42 -0700

    well let's see [p5 problems]

--- watersketch.js
+++ watersketch.js
@@ -1,41 +1,35 @@
-
 //source: https://www.youtube.com/watch?v=BZUdGqeOD0w
 //the dan sherman guy is crazy good
 //when he explains stuff, it makes sense, but when you try to explain it, you can't.
 
 //DIRECTIONS: click or drag mouse. water ripple effect.
 //NOTE: program loses it if you click or drag mouse off screen.
-var canvas
 
 var cols = 200;
 var rows = 200;
-Array [][] current;   //array#1 holds current state, can be any word
-Array [][] previous;  //array#2 hold state from previous frame, can be any word
+var[_] = current;   //array#1 holds current state, can be any word
+var[_] = previous;  //array#2 hold state from previous frame, can be any word
 
-Array dampening = 0.98; //ripple intensity (pick a number between 0 & 1)
+var dampening = 0.98; //ripple varensity (pick a number between 0 & 1)
                         //example: .9 = small, .999 = very large
 
-                       
-function windowResized(){
-	resizeCanvas();
-}
-function setup (){
- createCanvas (windowWidth,windowHeight);
+function setup () {
+  createCanvas(600,400);      //canvas createCanvas
   cols = width;
   rows = height;
-current = new Array[cols][rows];    //this is in setup so...
-previous = new Array[cols][rows];   //when you change the window size,
+current = new Array(cols)[rows];    //this is in setup so...
+previous = new Array(cols)[rows];   //when you change the window createCanvas,
                                     //the 2-dimensional arrays also change.
 }
 
-function mouseIsPressed() {
+function mousePressed() {
   current[mouseX][mouseY] = 255;     //ripple color is white
 }                                    
-function mouseIsDragged() {
+function mouseDragged() {
   current[mouseX][mouseY] = 255;     
 }
 
-function draw {
+function draw () {
   background(0);          //background is black
   loadPixels();
   for (var i = 1; i < cols-1; i++) {    // a method for looping through every non-edge element.
@@ -57,7 +51,12 @@ function draw {
 
 updatePixels();           //use value of current[i][j] to be pixel color
 
-Array [][] temp = previous;  //this is an algorithm to swap stuff around
+var [_] = temp; 
+temp = previous;  //this is an algorithm to swap stuff around
 previous = current;
 current = temp;
 }
+
+//WHY ISN'T IT WORKING OMG.
+//added in "_" inbetween bracket.
+//expanded var temp = prev, to var=temp & temp=prev.
